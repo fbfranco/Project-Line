2a484736c300416e39afb80180b875cfc5b631f4
Danner <dgalvarro@info-arch.com>
2018-06-04 13:55:51 -0400

    dfssdf

---
+++ ProjectLine.UI/src/app/components/projects/project-add/project-add.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-project-add',
+  templateUrl: './project-add.component.html',
+  styleUrls: ['./project-add.component.css']
+})
+export class ProjectAddComponent implements OnInit {  
+
+    date = new FormControl({ value: new Date(), disabled: true });
+    mindate =  new Date();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.date = "";
+  }
+
+}
+
