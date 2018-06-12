d394cd345bb71cd56b7e1d129dd44064677fe8d5
Sebastian Nicolas Quispe Gomez <sebastian.quispe.gomez@outlook.com>
2018-06-12 14:06:41 -0400

    UI - changes in routing.module.ts and components/objectives/etc completed

--- ProjectLine.UI/src/app/Routes/routing.module.ts
+++ ProjectLine.UI/src/app/Routes/routing.module.ts
@@ -5,12 +5,14 @@ import { ClientComponent } from '../components/clients/client/client.component';
 import { ProjectListComponent } from '../components/projects/project-list/project-list.component';
 import { ProjectAddComponent } from '../components/projects/project-add/project-add.component';
 import { PhasesFormComponent } from '../components/phases/phases-form/phases-form.component';
+import { ObjectivesComponent } from '../components/objectives/objectives.component';
 
 const routes: Routes = [
   { path: 'Project', component: ProjectListComponent },
   { path: 'Project/Add', component: ProjectAddComponent },
   { path: 'Project/Add/Phase', component: PhasesFormComponent },
-  { path: 'Client', component: PhasesFormComponent }
+  { path: 'Client', component: PhasesFormComponent },
+  { path: 'Objective', component: ObjectivesComponent }
 ];
 
 @NgModule({
