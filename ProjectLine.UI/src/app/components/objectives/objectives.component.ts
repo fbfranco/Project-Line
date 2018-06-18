771983beb2c29c4af2e5c2d80dc48ec0a285749a
Sebastian Nicolas Quispe Gomez <squispe@info-arch.com>
2018-06-18 14:05:17 -0400

    UI - Update: test in objectives components

--- ProjectLine.UI/src/app/components/objectives/objectives.component.ts
+++ ProjectLine.UI/src/app/components/objectives/objectives.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { startWith, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ObjectiveAddComponent } from './objective-add/objective-add.component';
 import { MatDialog } from '@angular/material';
-
-export interface StateGroup {
-  id: number;
-  letter: string;
-  names: string[];
-}
 
 @Component({
   selector: 'app-objectives',
@@ -19,49 +13,51 @@ export interface StateGroup {
 })
 export class ObjectivesComponent implements OnInit {
 
-  stateForm: FormGroup = this.fb.group({
-    stateGroup: '',
-  });
+  constructor(
 
-  stateGroups: StateGroup[] = [{
-    id: 1,
-    letter: 'A',
-    names: ['Alabama']
-  }, {
-    id: 2,
-    letter: 'C',
-    names: ['California']
-  }];
+    public dialog: MatDialog,
+    private fb: FormBuilder
 
-  stateGroupOptions: Observable<StateGroup[]>;
-
-  constructor(private fb: FormBuilder, public dialog: MatDialog) { }
-
-  ngOnInit() {
-    this.stateGroupOptions = this.stateForm.get('stateGroup')!.valueChanges
-      .pipe(
-        startWith(''),
-        map(val => this.filterGroup(val))
-      );
-  }
-
-  filterGroup(val: string): StateGroup[] {
-    if (val) {
-      return this.stateGroups
-        .map(group => ({ id: group.id, letter: group.letter, names: this._filter(group.names, val) }))
-        .filter(group => group.names.length > 0);
-    }
-
-    return this.stateGroups;
-  }
-
-  private _filter(opt: string[], val: string) {
-    const filterValue = val.toLowerCase();
-    return opt.filter(item => item.toLowerCase().startsWith(filterValue));
-  }
+  ) { }
 
   openDialog() {
-    const dialogRef = this.dialog.open(ObjectiveAddComponent);
+    console.log(this.formGroup);
+  }
+
+  myForm: FormControl;
+  filteredOptions: Observable<any[]>;
+
+  formGroup: FormGroup;
+  variable: string;
+
+  options: any[] = [
+    { "id": 1, "name": "colour", "cat": "red" },
+    { "id": 2, "name": "colour", "cat": "blue" },
+    { "id": 3, "name": "colour", "cat": "green" }
+  ];
+
+  ngOnInit(): void {
+    this.myForm = new FormControl();
+    this.filteredOptions = this.myForm.valueChanges
+      .pipe(
+        startWith(''),
+        map(val => this.findOption(val))
+      );
+
+    this.formGroup = this.fb.group({
+      title: '',
+      description: ''
+    });
+  }
+
+  findOption(val: string) {
+    this.variable=val;
+    console.log(val.toString()+"---------");
+    return this.options.filter((s) => new RegExp(val, 'gi').test(s.cat));
+  }
+
+  displayFn(country): string {
+    return country ? country.cat : country;
   }
 
 }
