0d1725e36552dbadf6dc0f6d89010ab7c98740e2
Daniel David Alvarez Rivero <dalvarez@info-arch.com>
2018-06-26 13:59:25 -0400

    Info-Changes

--- ProjectLine.UI/src/app/components/timeline/timeline.component.ts
+++ ProjectLine.UI/src/app/components/timeline/timeline.component.ts
@@ -9,30 +9,36 @@ import { PhaseService } from '../../services/phase.service';
 })
 export class TimelineComponent implements OnInit {
   PhaseModel: Phase[];
-  ProjectID: 1;
+  PhaseLocal: Phase[];
+  ProjectID: number;
+  Sw: boolean;
 
   constructor(private phaseService: PhaseService) {
-    // this.PhaseModel = [
-    //   {
-    //     PhaseID: 1, Title: 'Phase of Develoment', Description: 'This Phase is for develoment of project',
-    //     StartDate: new Date, EndDate: new Date, DemoUrl: 'http:://www.demo.com.bo'
-    //   },
-    //   {
-    //     PhaseID: 2, Title: 'Phase of Testing', Description: 'This Phase is for Testing of project',
-    //     StartDate: new Date, EndDate: new Date, DemoUrl: 'http:://www.demo.com.bo'
-    //   },
-    //   {
-    //     PhaseID: 3, Title: 'Phase of Mantenimiento', Description: 'This Phase is for Manteniemiento of project',
-    //     StartDate: new Date, EndDate: new Date, DemoUrl: 'http:://www.demo.com.bo'
-    //   }
-    // ];
+    this.ProjectID = 1;
+    this.Sw = true;
+    this.PhaseLocal = [
+      {
+        PhaseID: 1, Title: 'Phase of Develoment', Description: 'This Phase is for develoment of project',
+        StartDate: new Date, EndDate: new Date, DemoUrl: 'http:://www.demo.com.bo'
+      },
+      {
+        PhaseID: 2, Title: 'Phase of Testing', Description: 'This Phase is for Testing of project',
+        StartDate: new Date, EndDate: new Date, DemoUrl: 'http:://www.demo.com.bo'
+      },
+      {
+        PhaseID: 3, Title: 'Phase of Mantenimiento', Description: 'This Phase is for Manteniemiento of project',
+        StartDate: new Date, EndDate: new Date, DemoUrl: 'http:://www.demo.com.bo'
+      }
+    ];
 
-    // this.PhaseModel = phaseService.getPhasesList(this.ProjectID);
+    // this.PhaseLocal = phaseService.getPhasesList(this.ProjectID);
+  }
+  ngOnInit() {
+    this.displayPhasesOnTimeLine(this.ProjectID);
   }
 
-  ngOnInit() {
-    // console.table(this.PhaseModel);
-    this.phaseService.getPhasesList(2).subscribe((data: Phase[]) => {
+  displayPhasesOnTimeLine(projectID: number) {
+    this.phaseService.getPhasesList(projectID).subscribe((data: Phase[]) => {
       this.PhaseModel = data;
       console.table(this.PhaseModel);
     }, error => {
