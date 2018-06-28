c1f677668b23d099ad3ff42beaa1d60402d99d37
Danner <dgalvarro@info-arch.com>
2018-06-28 08:47:46 -0400

    Story UI/MT: Project Timeline - Display Project start and end dates complete.

--- ProjectLine.UI/src/app/components/timeline/timeline/timeline.component.ts
+++ ProjectLine.UI/src/app/components/timeline/timeline/timeline.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Phase } from '../../../models/phase.model';
 import { PhaseService } from '../../../services/phase.service';
+import { HelperService } from '../../../services/helper.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { MatDialog, MatSnackBar } from '@angular/material';
 
@@ -19,14 +20,13 @@ export class TimelineComponent implements OnInit {
   PhaseModel: Phase[];
   ProjectID: number;
 
-    // List Projects
-    ListProjects: Project[];
-    EndDate: string;
-    StartDate: string;
-    Hide: boolean;
-    values = '';
-
-  constructor(public projectService: ProjectService, private phaseService: PhaseService) { }
+  // List Projects
+  ListProjects: Project[];
+  StartDate: any;
+  EndDate: any;
+  Hide: boolean;
+  values = '';
+  constructor(public projectService: ProjectService, private phaseService: PhaseService, private helperService: HelperService) { }
 
   ngOnInit() {
     $('.VivaTimeline').vivaTimeline();
@@ -42,8 +42,12 @@ export class TimelineComponent implements OnInit {
 
   // Get the dates of the selected project
   projectChanged(event): void {
-    this.EndDate = event.option.value.EndDate;
-    this.StartDate = event.option.value.StartDate;
+    this.StartDate = this.helperService.DateFormat( new Date(event.option.value.StartDate));
+    this.EndDate = this.helperService.DateFormat(event.option.value.EndDate);
+    console.log(this.StartDate);
+    /* this.EndDate = event.option.value.EndDate;
+    this.StartDate = event.option.value.StartDate; */
+
     this.ProjectID = event.option.value.ProjectID;
     this.displayPhasesOnTimeLine(this.ProjectID);
     this.Hide = true;
@@ -51,16 +55,16 @@ export class TimelineComponent implements OnInit {
 
   getProjectList() {
     this.projectService.getProjectsList().subscribe((datalist: Project[]) => {
-    this.ListProjects = datalist;
+      this.ListProjects = datalist;
     }, error => {
     });
   }
 
   inputEmpty(event: any) {
-     if (event !== '' ) {
+    if (event !== '') {
       this.Hide = false;
       console.log(this.StartDate);
-     }
+    }
   }
 
 
