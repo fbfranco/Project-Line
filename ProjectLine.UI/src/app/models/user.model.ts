291b994a6d31ece3916acef57fffcba0201d043a
Daniel David Alvarez Rivero <dalvarez@info-arch.com>
2018-07-13 13:02:00 -0400

    UI - Componenst Timeline and LisUser Integrate

--- ProjectLine.UI/src/app/models/user.model.ts
+++ ProjectLine.UI/src/app/models/user.model.ts
@@ -1,15 +1,15 @@
 // import { Role } from './role.model';
 
 export class User {
-    UserID: number;
-    Name: string;
-    LastName: string;
-    Email: string;
-    Enterprise: string;
-    Address: string;
-    Status: boolean;
-    Mobile: number;
-    Phone: number;
+  UserID: number;
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  Company: string;
+  Address: string;
+  Active: boolean;
+
+  RoleID: number;
 
   /*   RoleID?: string; */
   // Roles?: Role[];
