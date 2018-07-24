b371c471406e6457d65c462903c77b3359543295
BISMARCK-PC <bfranco@info-arch.com>
2018-07-24 07:13:51 -0400

    Add-Edit project with client and owner Aligne data phase in the timeline

--- ProjectLine.UI/src/app/validators/client-owner-autocomplete.validator.ts
+++ ProjectLine.UI/src/app/validators/client-owner-autocomplete.validator.ts
@@ -2,16 +2,13 @@ import { User } from '../models/user.model';
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 
 export function isSelectedValid(ListFiltered: User[]): ValidatorFn {
-  if (ListFiltered === undefined) {
-    alert('hola mundo');
-  } else if (ListFiltered !== undefined) {
-    alert('hola mundo 22');
+  if (ListFiltered !== undefined) {
     const ListID = [];
     ListFiltered.forEach(element => {
       ListID.push(element.UserID.toString());
     });
     return function (control: AbstractControl): {[Key: string]: any} {
-      if (control.dirty && typeof control.value === 'string') {
+      if (control.dirty && typeof control.value === 'string' && control.value.length > 0) {
         return ListID.lastIndexOf(control.value) !== -1 ? null : {'isSelectedValid': true};
       } else {
         return null;
