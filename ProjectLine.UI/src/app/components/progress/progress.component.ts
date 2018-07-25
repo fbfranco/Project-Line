eef1cd8c80eb0fc197e5cca1fa2034fff2c5c621
Sebastian Quispe <squispe@info-arch.com>
2018-07-25 11:07:46 -0400

    update

--- ProjectLine.UI/src/app/components/progress/progress.component.ts
+++ ProjectLine.UI/src/app/components/progress/progress.component.ts
@@ -15,59 +15,36 @@ export class ProgressComponent implements OnInit {
 
   private objectiveNumber: number;
   private objectiveCompleted: number;
-  private progressPercentage: number;
+  private progressCompleted: number;
+  private progressPending: number;
 
   constructor(
     private helperService: HelperService
   ) { }
 
   ngOnInit() {
-    this.resetNumbers();
-    this.showProgress('rgb(80, 80, 80)');
+    this.startNumbers();
+    this.calculateProgress();
+    this.showProgressComplete(2);
   }
 
-  sidenavState() {
-    const percentage = document.getElementsByClassName('percentage') as HTMLCollectionOf<HTMLElement>;
-    const legend = document.getElementsByClassName('legend') as HTMLCollectionOf<HTMLElement>;
-    if (percentage.length !== 0) {
-      if (this.helperService.SlideMenu.opened) {
-        percentage[0].setAttribute('style', 'position: fixed; top:200px; left:240px;');
-        legend[0].setAttribute('style', 'position: fixed; top:520px; left:332px;');
-      } else {
-        percentage[0].setAttribute('style', 'position: fixed; top:200px; left:2px;');
-        legend[0].setAttribute('style', 'position: fixed; top:520px; left:92px;');
-      }
-    }
-  }
-
-  resetNumbers() {
+  startNumbers() {
     this.objectiveNumber = 0;
     this.objectiveCompleted = 0;
-    this.progressPercentage = 0;
+    this.progressCompleted = 0;
+    this.progressPending = 0;
   }
 
-  showProgress(color) {
-    if (color === 'rgb(80, 80, 80)') {
-      $('#progress-circle').circliful({
-        backgroundBorderWidth: 14,
-        replacePercentageByText: '',
-        backgroundColor: 'rgb(80, 80, 80)',
-        percent: this.progressPercentage,
-      });
-    } else {
-      $('#progress-circle').empty();
-      $('#progress-circle').circliful({
-        animation: 1,
-        animationStep: 5,
-        foregroundBorderWidth: 14,
-        backgroundBorderWidth: 14,
-        fontColor: color,
-        percentageTextSize: 32,
-        foregroundColor: color,
-        backgroundColor: 'rgb(80, 80, 80)',
-        percent: this.progressPercentage,
-      });
+  calculateProgress() {
+    for (const phase in this.phasesProject) {
+      if (this.phasesProject.hasOwnProperty(phase)) {
+        this.countObjectives(this.phasesProject[phase].Objectives);
+      }
     }
+    if (this.objectiveNumber > 0) {
+      this.progressCompleted = (this.objectiveCompleted * 100) / this.objectiveNumber;
+    }
+    this.progressPending = 100 - this.progressCompleted;
   }
 
   countObjectives(phasesProject) {
@@ -82,21 +59,47 @@ export class ProgressComponent implements OnInit {
     }
   }
 
-  calculateProgress(val) {
-    this.resetNumbers();
-    for (const phase in this.phasesProject) {
-      if (this.phasesProject.hasOwnProperty(phase)) {
-        this.countObjectives(this.phasesProject[phase].Objectives);
+  showProgressComplete(load) {
+    $('#progress-circle').empty();
+    $('#progress-circle').circliful({
+      animation: load,
+      animationStep: 5,
+      foregroundBorderWidth: 14,
+      backgroundBorderWidth: 14,
+      fontColor: 'rgb(6, 170, 214)',
+      percentageTextSize: 32,
+      foregroundColor: 'rgb(6, 170, 214)',
+      backgroundColor: 'rgb(80, 80, 80)',
+      percent: this.progressCompleted.toFixed(),
+    });
+  }
+
+  showProgressPending(load) {
+    $('#progress-circle').empty();
+    $('#progress-circle').circliful({
+      animation: load,
+      animationStep: 5,
+      foregroundBorderWidth: 14,
+      backgroundBorderWidth: 14,
+      fontColor: 'rgb(211, 131, 12)',
+      percentageTextSize: 32,
+      foregroundColor: 'rgb(211, 131, 12)',
+      backgroundColor: 'rgb(80, 80, 80)',
+      percent: this.progressPending.toFixed(),
+    });
+  }
+
+  sidenavState() {
+    const percentage = document.getElementsByClassName('percentage') as HTMLCollectionOf<HTMLElement>;
+    const legend = document.getElementsByClassName('legend') as HTMLCollectionOf<HTMLElement>;
+    if (percentage.length !== 0) {
+      if (this.helperService.SlideMenu.opened) {
+        percentage[0].setAttribute('style', 'position: fixed; top:200px; left:240px;');
+        legend[0].setAttribute('style', 'position: fixed; top:520px; left:332px;');
+      } else {
+        percentage[0].setAttribute('style', 'position: fixed; top:200px; left:2px;');
+        legend[0].setAttribute('style', 'position: fixed; top:520px; left:92px;');
       }
-    }
-    if (this.objectiveNumber > 0) {
-      this.progressPercentage = (this.objectiveCompleted * 100) / this.objectiveNumber;
-    }
-    if (val === 1) {
-      this.showProgress('rgb(6, 170, 214)');
-    } else {
-      this.progressPercentage = 100 - this.progressPercentage;
-      this.showProgress('rgb(211, 131, 12)');
     }
   }
 
