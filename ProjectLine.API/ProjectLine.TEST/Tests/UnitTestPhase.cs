0c984d55fbc5999974b5f5af89cb03064abc0d68
Sebastian Quispe <squispe@info-arch.com>
2018-08-13 12:13:33 -0400

    update UnitTests and remove ^

--- ProjectLine.API/ProjectLine.TEST/Tests/UnitTestPhase.cs
+++ ProjectLine.API/ProjectLine.TEST/Tests/UnitTestPhase.cs
@@ -13,126 +13,65 @@ namespace ProjectLine.TEST.Tests
     [TestClass]
     public class UnitTestPhase
     {
-        PhaseRepository repo;
-        Phase phase = new Phase();
-        ProjectLineContext Context = new ProjectLineContext();
-
-        ProjectRepository repos = new ProjectRepository();
+        PhaseRepository repository;
 
         [TestInitialize]
         public void TestSetup()
         {
             InitializeDB db = new InitializeDB();
             Database.SetInitializer(db);
-            repo = new PhaseRepository();
+            repository = new PhaseRepository();
         }
 
         [TestMethod]
-        public void IsPhaseInitializableDB()
+        public void IsInitializableDB()
         {
-            var result = repo.GetPhases();
+            var result = repository.GetPhases();
             Assert.IsNotNull(result);
         }
 
-        [TestMethod]
-        public void AddProject()
-        {
-            Project name = new Project()
-            {
-
-                Title = "David",
-                Description = "Descripcion1",
-                StartDate = new DateTime(2017, 08, 09),
-                EndDate = new DateTime(2017, 08, 09),
-                StatusID = 5
-            };
-
-            Phase NamePhase = new Phase()
-            {
-                Title = "FaseDavid",
-                Description = "Descripcion1",
-                StartDate = new DateTime(2017, 08, 09),
-                EndDate = new DateTime(2017, 08, 09),
-                DemoUrl = "Demo1"
-            };
-
-
-            List<Phase> ListPhase = new List<Phase>();
-            ListPhase.Add(NamePhase);
-
-            Phase NamePhase2 = new Phase()
-            {
-                Title = "FaseEider",
-                Description = "Descripcion2",
-                StartDate = new DateTime(2019, 08, 09),
-                EndDate = new DateTime(2019, 08, 09),
-                DemoUrl = "Demo1"
-            };
-            ListPhase.Add(NamePhase2);
-
-            ProjectViewModel ViewModel = new ProjectViewModel()
-            {
-                Project = name,
-                Phases = ListPhase
-            };
-            // repos.Create(ViewModel);
-        }
-
-        ObjectiveRepository obj = new ObjectiveRepository();
+        RoleRepository objrole = new RoleRepository();
 
         [TestMethod]
-        public void AddObjective()
+        public void AddRoles()
         {
-            Objective o = new Objective()
-            {
-                Title = "David",
-                Description = "Descripcion1",
-                Completed = true,
-                PhaseID = 1
-            };
-            obj.Create(o);
-        }
-
-        RoleRepository objrol = new RoleRepository();
-        [TestMethod]
-        public void AddRol()
-        {
-            Role r1 = new Role()
+            Role role1 = new Role()
             {
                Title = "Administrator",
-               Description="Administrador"
+               Description="Admin"
             };
-            objrol.Create(r1);
+            objrole.Create(role1);
 
-            Role r2 = new Role()
+            Role role2 = new Role()
             {
                Title = "Product Owner",
                Description = "PO"
             };
-            objrol.Create(r2);
+            objrole.Create(role2);
 
-            Role r3 = new Role()
+            Role role3 = new Role()
             {
                Title = "Client",
-               Description = "Cliente"
+               Description = "Client"
             };
-            objrol.Create(r3);
+            objrole.Create(role3);
         }
 
         UserRepository objuser = new UserRepository();
+
         [TestMethod]
         public void AddUser()
         {
             User user1 = new User()
             {
-                FirstName = "Danner",
-                LastName = "Galvarro Vaca",
-                Email = "dannergal@hotmail.com",
-                Company = "Apple",
-                Address = "Cupertino, CA",
-                Phone = "1-800-345-12345",
-                Mobile = "+(123)123-456-7890",
-                Password = "Danner12",
+                FirstName = "Admin",
+                LastName = "System",
+                Email = "admin@project.com",
+                Company = "System",
+                Address = " ",
+                Phone = " ",
+                Mobile = " ",
+                Password = "Admin123",
                 Active = true,
                 RoleID = 1
             };
@@ -141,6 +80,7 @@ namespace ProjectLine.TEST.Tests
 
         PermissionsRepository objPermission = new PermissionsRepository();
         PermissionRoleRepository objPermissionRole = new PermissionRoleRepository();
+
         [TestMethod]
         public void AddPermissions()
         {
